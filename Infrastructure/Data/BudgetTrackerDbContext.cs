e4639fd859c7488ce8d08ab96b2e4e3d6a4ea3ff
zymeng1998 <zymeng1998@gmail.com>
2021-11-15 14:05:20 -0800

    Add project files.

---
+++ Infrastructure/Data/BudgetTrackerDbContext.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+
+    public class BudgetTrackerDbContext : DbContext
+    {
+        public BudgetTrackerDbContext(DbContextOptions<BudgetTrackerDbContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Expenditure> Expenditures { get; set; }
+    }
+}
