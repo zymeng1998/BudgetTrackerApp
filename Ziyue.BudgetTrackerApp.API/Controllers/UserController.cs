5e5e1f354c7d2010a7edcc5983d6eb1416399e64
zymeng1998 <zymeng1998@gmail.com>
2021-11-15 21:43:40 -0800

    create usercontroller

---
+++ Ziyue.BudgetTrackerApp.API/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using ApplicationCore.ServiceInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ziyue.BudgetTrackerApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("Detail/{id:int}")]
+        // https://localhost/api/user/detail/{id}/
+
+        public async Task<IActionResult> GetUserDetail(int id) {
+            var userDetailResponse = await _userService.GetUserDetail(id);
+            if (userDetailResponse != null) {
+                return Ok(userDetailResponse);
+            }
+            return NotFound("User Not Found");
+        }
+    }
+}
